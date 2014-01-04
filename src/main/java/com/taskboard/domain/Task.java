c14e532e90177dd9d1538cc3b7856b98513e7d1c
Shekhar Gulati <shekhargulati84@gmail.com>
2014-01-04 18:44:57 +0530

    created domain model, also added hot_deploy marker

---
+++ src/main/java/com/taskboard/domain/Task.java
@@ -0,0 +1,160 @@
+package com.taskboard.domain;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: shekhargulati
+ * Date: 04/01/14
+ * Time: 3:16 PM
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "tasks")
+@Access(AccessType.FIELD)
+public class Task {
+
+    @NotNull
+    @Id
+    @TableGenerator(table = "ID_GEN",name = "task_gen",initialValue = 10000,allocationSize = 100)
+    @GeneratedValue(generator = "task_gen")
+    private Long id;
+
+    @Column
+    @NotNull
+    @Size(max=140)
+    private String task;
+
+    @Column
+    @NotNull
+    @ElementCollection
+    @CollectionTable("TaskTypes")
+    private List<String> tags = new ArrayList<String>();
+
+    @Column
+    @NotNull
+    @Future
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dueDate;
+
+    @NotNull
+    @Min(0)
+    @Max(10)
+    private int points;
+
+    @Column
+    @NotNull
+    private boolean done = false;
+
+    @Column(updatable = false)
+    @NotNull
+    @Temporal(TemporalType.DATE)
+    private Date createdOn = new Date();
+
+    @Column
+    @NotNull
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date finishedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "taskboard_id")
+    @NotNull
+    private Taskboard taskboard;
+
+
+    protected Task() {
+    }
+
+    public Task(String task, List<String> tags, Date dueDate, int points) {
+        this.task = task;
+        this.tags = tags;
+        this.dueDate = dueDate;
+        this.points = points;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public Date getCreatedOn() {
+        return createdOn;
+    }
+
+    public Date getFinishedAt() {
+        return finishedAt;
+    }
+
+    public void setFinishedAt(Date finishedAt) {
+        this.finishedAt = finishedAt;
+    }
+
+    public Taskboard getTaskboard() {
+        return taskboard;
+    }
+
+    public void setTaskboard(Taskboard taskboard) {
+        this.taskboard = taskboard;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", task='" + task + '\'' +
+                ", tags=" + tags +
+                ", dueDate=" + dueDate +
+                ", points=" + points +
+                ", done=" + done +
+                ", createdOn=" + createdOn +
+                ", finishedAt=" + finishedAt +
+                '}';
+    }
+}
---
+++ src/main/java/com/taskboard/domain/Taskboard.java
@@ -0,0 +1,139 @@
+package com.taskboard.domain;
+
+import javax.persistence.*;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: shekhargulati
+ * Date: 04/01/14
+ * Time: 3:07 PM
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "taskboards")
+@Access(AccessType.FIELD)
+public class Taskboard {
+    @Id
+    @NotNull
+    @TableGenerator(table = "ID_GEN", name = "taskboard_gen",allocationSize = 100,initialValue = 1)
+    @GeneratedValue(generator = "taskboard_gen")
+    private Long id;
+
+    @Column
+    @NotNull
+    @Size(max = 140)
+    private String name;
+
+    @Column
+    @Size(max=4000)
+    private String description;
+
+    @Column(updatable = false)
+    @NotNull
+    @Temporal(TemporalType.DATE)
+    private Date createdOn = new Date();
+
+    @Column
+    @Future
+    @NotNull
+    @Temporal(TemporalType.DATE)
+    private Date start;
+
+    @Column
+    @Future
+    @Temporal(TemporalType.DATE)
+    private Date end;
+
+    @Column
+    @Max(100)
+    @NotNull
+    private int capacity;
+
+    @OneToMany(mappedBy = "taskboard_id")
+    private List<Task> tasks = new ArrayList<Task>();
+
+
+    protected Taskboard() {
+    }
+
+
+    public Taskboard(String name, String description, Date start, Date end, int capacity) {
+        this.name = name;
+        this.description = description;
+        this.start = start;
+        this.end = end;
+        this.capacity = capacity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreatedOn() {
+        return createdOn;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    @Override
+    public String toString() {
+        return "Taskboard{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", createdOn=" + createdOn +
+                ", start=" + start +
+                ", end=" + end +
+                ", capacity=" + capacity +
+                '}';
+    }
+}
