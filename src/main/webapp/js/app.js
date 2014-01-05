3e6cdc2a5dd45cc30c4a2d4d9f8f82701bcf4f3e
Shekhar Gulati <shekhargulati84@gmail.com>
2014-01-05 23:20:12 +0530

    fixed namespacing issue with Controllers.. gave module name,etc

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,73 +1,65 @@
-angular.module("taskboard.services", ["ngRoute","ngResource"]).
-    factory('taskboard', function ($resource) {
-        var taskboard = $resource('http://taskboard-shekhargulati.rhcloud.com/api/v1/taskboards/:taskboardId', {taskboardId: '@id'});
-        taskboard.prototype.isNew = function(){
-            return (typeof(this.id) === 'undefined');
-        }
-        return taskboard;
-    }).
-    factory('task',function($resource,$routeParams){
-        var taskboardId = $routeParams.taskboardId;
-        var task = $resource('http://taskboard-shekhargulati.rhcloud.com/api/v1/taskboards/:taskboardId/tasks/:taskId', {taskboardId: taskboardId,taskId:"@id"});
-        task.prototype.isNew = function(){
-            return (typeof(this.id) === 'undefined');
-        }
-        return task;
-    });
+var taskboardServicesModlule = angular.module("taskboard.services", ["ngRoute", "ngResource"]);
 
-angular.module("taskboard", ["taskboard.services"]).
+taskboardServicesModlule.factory('Taskboard', function ($resource) {
+    var taskboard = $resource('http://taskboard-shekhargulati.rhcloud.com/api/v1/taskboards/:taskboardId', {taskboardId: '@id'});
+    taskboard.prototype.isNew = function () {
+        return (typeof(this.id) === 'undefined');
+    }
+    return taskboard;
+});
+
+taskboardServicesModlule.factory('Task', function ($resource, $routeParams) {
+    var taskboardId = $routeParams.taskboardId;
+    var task = $resource('http://taskboard-shekhargulati.rhcloud.com/api/v1/taskboards/:taskboardId/tasks/:taskId', {taskboardId: taskboardId, taskId: "@id"});
+    task.prototype.isNew = function () {
+        return (typeof(this.id) === 'undefined');
+    }
+    return task;
+});
+
+var taskboardModule = angular.module("taskboard", ["taskboard.services"]).
     config(function ($routeProvider) {
         $routeProvider
-            .when('/', {templateUrl: 'views/taskboard/list.html', controller: taskboardListController})
-            .when('/taskboards/new', {templateUrl: 'views/taskboard/create.html', controller: taskboardCreateController})
-            .when('/taskboards/:taskboardId', {templateUrl: 'views/taskboard/detail.html', controller: taskboardDetailController})
-            .when('/taskboards/:taskboardId/tasks/new',{templateUrl: 'views/task/create.html', controller: taskCreateController});
+            .when('/', {templateUrl: 'views/taskboard/list.html', controller: taskboardModule.taskboardListController})
+            .when('/taskboards/new', {templateUrl: 'views/taskboard/create.html', controller: taskboardModule.taskboardCreateController})
+            .when('/taskboards/:taskboardId', {templateUrl: 'views/taskboard/detail.html', controller: taskboardModule.taskboardDetailController})
+            .when('/taskboards/:taskboardId/tasks/new', {templateUrl: 'views/task/create.html', controller: taskboardModule.taskCreateController});
     });
 
 
-
-
-function taskboardListController($scope, taskboard) {
-    $scope.taskboards = taskboard.query();
-
-
+taskboardModule.taskboardListController = function ($scope, Taskboard) {
+    $scope.taskboards = Taskboard.query();
 }
 
-function taskboardCreateController($scope, $routeParams, $location, taskboard) {
-
-    $scope.taskboard = new taskboard();
-
+taskboardModule.taskboardCreateController = function ($scope, $routeParams, $location, Taskboard) {
+    $scope.taskboard = new Taskboard();
     $scope.save = function () {
-    	$scope.taskboard.$save(function (taskboard, headers) {
-    		toastr.success("Created New taskboard");
+        $scope.taskboard.$save(function (taskboard, headers) {
+            toastr.success("Created New taskboard");
             $location.path('/');
         });
     };
 }
 
 
-function taskboardDetailController($scope, $routeParams, $location, taskboard) {
+taskboardModule.taskboardDetailController = function ($scope, $routeParams, $location, Taskboard) {
     var taskboardId = $routeParams.taskboardId;
-
-    $scope.taskboard = taskboard.get({taskboardId: taskboardId});
-
+    $scope.taskboard = Taskboard.get({taskboardId: taskboardId});
 }
 
 
-function taskCreateController($scope, $routeParams, $location, task) {
+taskboardModule.taskCreateController = function ($scope, $routeParams, $location, Task) {
     var taskboardId = $routeParams.taskboardId;
-
-    $scope.task = new task();
-
+    $scope.task = new Task();
     $scope.save = function () {
         var task = $scope.task;
-        if(!(task['tags'] instanceof Array)){
+        if (!(task['tags'] instanceof Array)) {
             task['tags'] = task['tags'].split(",");
             $scope.task = task;
         }
         $scope.task.$save(function (task, headers) {
             toastr.success("Created New Task");
-            $location.path('/taskboards/'+taskboardId);
+            $location.path('/taskboards/' + taskboardId);
         });
     };
 }
